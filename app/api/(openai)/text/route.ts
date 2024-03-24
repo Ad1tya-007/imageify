e266a161b7fb117aa11a72668f07d35e42171d1b
Ad1tya-007 <adityakul0314@gmail.com>
2024-03-24 10:15:42 -0700

    update

--- app/api/(openai)/text/route.ts
+++ app/api/(openai)/text/route.ts
@@ -1,43 +1,43 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import OpenAI from "openai";
-import { NextResponse } from "next/server";
-import cloudinary from "cloudinary";
+import { NextApiRequest, NextApiResponse } from 'next';
+import OpenAI from 'openai';
+import { NextResponse } from 'next/server';
+import cloudinary from 'cloudinary';
 
 const openai = new OpenAI({
-	apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 cloudinary.v2.config({
-	cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-	const chunks = [];
-	for await (const chunk of req.body) {
-		chunks.push(chunk);
-	}
-	const body = Buffer.concat(chunks).toString("utf-8");
+export async function POST(req: any, res: NextApiResponse) {
+  const chunks = [];
+  for await (const chunk of req.body) {
+    chunks.push(chunk);
+  }
+  const body = Buffer.concat(chunks).toString('utf-8');
 
-	// Now `body` is a string that contains your JSON data
-	const jsonData = JSON.parse(body);
+  // Now `body` is a string that contains your JSON data
+  const jsonData = JSON.parse(body);
 
-	const cloudinaryResp = await cloudinary.v2.api.resource(jsonData.publicId, {
-		resource_type: "video",
-	});
+  const cloudinaryResp = await cloudinary.v2.api.resource(jsonData.publicId, {
+    resource_type: 'video',
+  });
 
-	console.log(cloudinaryResp.tags);
+  console.log(cloudinaryResp.tags);
 
-	const query = `Could you generate a short story with the follow description: ${
-		jsonData.description
-	} and the given tags ${cloudinaryResp.tags.join(", ")}?`;
-	const completion = await openai.chat.completions.create({
-		messages: [{ role: "user", content: query ?? "Tell me about yourself" }],
-		model: "gpt-3.5-turbo",
-	});
+  const query = `Could you generate a short story with the follow description: ${
+    jsonData.description
+  } and the given tags ${cloudinaryResp.tags.join(', ')}?`;
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: query ?? 'Tell me about yourself' }],
+    model: 'gpt-3.5-turbo',
+  });
 
-	const data = completion.choices[0].message.content;
-	console.log(data);
-	return NextResponse.json({ story: data });
+  const data = completion.choices[0].message.content;
+  console.log(data);
+  return NextResponse.json({ story: data });
 }
